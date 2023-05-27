8627cf51ddc456a9f5058d1d62dac0ca918be3df
amane <atapradana@gmail.com>
2023-05-27 05:37:27 +0000

    Update framework version and add Psr7Bridge class
    * Add Psr7Bridge class in lib for bridging CI4 request-response to PSR-7
    * Update framework version to avoid known vulnerability (CVE-2023-32692)

---
+++ app/Libraries/Psr7Bridge.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Libraries;
+
+use CodeIgniter\Config\Services;
+use CodeIgniter\HTTP\IncomingRequest;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\ResponseInterface;
+use CodeIgniter\HTTP\Response as CI4Response;
+
+class Psr7Bridge {
+    /**
+     * Creates a PSR-7 Server Request instance from a CodeIgniter4 IncomingRequest object.
+     */
+    public static function createServerRequest(IncomingRequest $request) {
+        $factory    = new Psr17Factory();
+        $psrRequest = $factory->createServerRequest(
+            $request->getMethod(true),
+            $request->getUri()->__toString(),
+            $request->getServer()
+        );
+
+        // Add headers
+        foreach ($request->headers() as $header) {
+            try {
+                $psrRequest = $psrRequest->withHeader($header->getName(), $header->getValue());
+            } catch (\Throwable $th) {
+                // ignore invalid, for convenience
+            }
+        }
+
+        // Fetch query params from URI
+        parse_str($request->getUri()->getQuery(), $queryArr);
+
+        return $psrRequest
+            ->withBody($factory->createStream($request->getBody() ?? 'php://memory'))
+            ->withCookieParams($request->getCookie())
+            ->withUploadedFiles($request->getFiles())
+            ->withQueryParams($queryArr)
+            ->withParsedBody($request->getVar());
+    }
+
+    /**
+     * Creates a PSR-7 Response instance from a CodeIgniter4 Response object.
+     */
+    public static function createResponse(CI4Response $response) {
+        $factory     = new Psr17Factory();
+        $psrResponse = $factory->createResponse($response->getStatusCode(), $response->getReasonPhrase());
+
+        // Add headers and cookies
+        foreach ($response->headers() as $header) {
+            try {
+                $psrResponse = $psrResponse->withHeader($header->getName(), $header->getValue());
+            } catch (\Throwable $th) {
+                // ignore invalid, for convenience
+            }
+        }
+        if (!empty($response->getCookies())) {
+            $cookies = [];
+            foreach ($response->getCookies() as $cookie) {
+                $cookies[] = $cookie->__toString();
+            }
+            $psrResponse->withHeader('Set-Cookie', $cookies);
+        }
+
+        return $psrResponse
+            ->withProtocolVersion($response->getProtocolVersion())
+            ->withBody($factory->createStream($response->getBody() ?? 'php://memory'));
+    }
+
+    /**
+     * Creates a CI4 Response instance from a PSR-7 Response instance.
+     */
+    public static function createCI4Response(ResponseInterface $response) {
+        $ci4Response = Services::response();
+
+        // Add header
+        foreach ($response->getHeaders() as $name => $value) {
+            try {
+                $ci4Response->setHeader($name, $value);
+            } catch (\Throwable $th) {
+                // ignore invalid, for convenience
+            }
+        }
+
+        return $ci4Response
+            ->setProtocolVersion($response->getProtocolVersion())
+            ->setStatusCode($response->getStatusCode(), $response->getReasonPhrase())
+            ->setBody($response->getBody()->getContents());
+    }
+}
